0872d267e884eccf3a6de015418542a8a81f0888
snaens <snaens@snaens.net>
2025-10-15 22:35:05 +0200

    initial commit
    set up ROS project for C++ and Python

---
+++ demo_sensor_py/scripts/demo_sensor_node.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+# from bento_sensors.module_to_import import ...
+
+print("Hello World")
---
+++ mlx90640/src/mlx90640_node.cpp
@@ -0,0 +1,6 @@
+#include "bento_sensors/cpp_header.hpp" //includes iostream
+
+int main() {
+  std::cout << "Hello World" << std::endl;
+  return 0;
+}
