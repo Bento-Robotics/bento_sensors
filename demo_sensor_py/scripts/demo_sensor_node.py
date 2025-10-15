36a93d08e79230bfd14639e3cf513e1d85fa59a3
snaens <snaens@snaens.net>
2025-10-15 23:14:37 +0200

    add demo_sensor_py example

--- demo_sensor_py/scripts/demo_sensor_node.py
+++ demo_sensor_py/scripts/demo_sensor_node.py
@@ -1,5 +1,40 @@
 #!/usr/bin/env python3
 
-# from bento_sensors.module_to_import import ...
+import rclpy
+from rclpy.executors import ExternalShutdownException
+from rclpy.node import Node
 
-print("Hello World")
+from std_msgs.msg import String
+
+from modules.demo_sensor_lib import data
+
+
+class DemoSensor(Node):
+
+    def __init__(self):
+        super().__init__('demo_sensor')
+        self.publisher_ = self.create_publisher(String, 'data', 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = data
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+
+
+def main(args=None):
+    try:
+        rclpy.init(args=args)
+        sensor_demo = DemoSensor()
+
+        rclpy.spin(sensor_demo)
+    except (KeyboardInterrupt, ExternalShutdownException):
+        pass
+    finally:
+        sensor_demo.destroy_node()
+
+
+if __name__ == '__main__':
+    main()
