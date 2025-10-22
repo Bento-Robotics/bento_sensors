58255e8c21e6d7cd7003a21826ee57f46552fce8
Bento-Box <bento.robotics@gmail.com>
2025-10-22 23:12:45 +0200

    complete support for ina219 power sensor

--- powa_ina219/ina219_node.py
+++ powa_ina219/ina219_node.py
@@ -6,14 +6,14 @@ from rclpy.node import Node
 
 from sensor_msgs.msg import BatteryState
 
-# for locating text display font file
-from os import path
+# For float 'NaN'
+import math
 
 # Import all board pins.
 from board import SCL, SDA
 import busio
 
-import adafruit_ina219
+from adafruit_ina219 import INA219, ADCResolution
 
 
 class ina219_Node(Node):
@@ -21,9 +21,11 @@ class ina219_Node(Node):
     def __init__(self):
         # Create the I2C interface.
         self.i2c = busio.I2C(SCL, SDA)
-
-        self.sensor = adafruit_ina219.INA219(self.i2c)
-
+        # Set up INA219 sensor
+        self.sensor = INA219(self.i2c)
+        # Configure INA219 to only take 2 samples per measurement
+        self.sensor.bus_adc_resolution = ADCResolution.ADCRES_12BIT_2S
+        self.sensor.shunt_adc_resolution = ADCResolution.ADCRES_12BIT_2S
 
         # Initialize ROS node
         super().__init__('powa_ina219')
@@ -34,12 +36,38 @@ class ina219_Node(Node):
 
     def timer_callback(self):
         msg = BatteryState()
-        msg.voltage = float(self.sensor.bus_voltage)
-        msg.current = float(self.sensor.current)
-        # 6s battery, each 3.0V ~ 4.2V
-        msg.percentage = float( (msg.voltage - 3.0*6) * (100 - 0) / (4.2*6 - 3.0*6) + 0 )
+        # Stuff we don't measure - ROS standards say these should all be NaN
+        msg.charge = msg.capacity = msg.design_capacity = math.nan
+        msg.cell_voltage = [math.nan] * 6
+        # This battery (should) always be present
+        msg.present = True
+        # This is a Li-Ion battery
+        msg.power_supply_technology = BatteryState.POWER_SUPPLY_TECHNOLOGY_LION
+        # Guessing things are fine.
+        # If something interesting is going on, we change it later. This is the default.
+        msg.power_supply_status = BatteryState.POWER_SUPPLY_STATUS_NOT_CHARGING
+        msg.power_supply_health = BatteryState.POWER_SUPPLY_HEALTH_GOOD
+
+        # Voltage
+        msg.voltage = float(f'{self.sensor.bus_voltage:.2f}')
+        # Current - inverted: "as seen from battery"
+                        # cut all decimal digits, as they are mostly noise
+        msg.current = - float(f'{(self.sensor.current/1000):.0f}')
+        # Percentage 0~1 - 6s battery, each 3.0V ~ 4.2V
+        msg.percentage = float(f'{(msg.voltage - 3.0*6) / (4.2*6 - 3.0*6):.2f}')
+
+        if msg.voltage < 1.1 and msg.voltage > 0.7:
+            self.get_logger().warn('This is around the measuring voltage if the INA219 has nothing connected. You may have a floating ground connection.')
+        if msg.voltage < (6 * 3.0): # lower limit
+            self.get_logger().error('Battery Empty!')
+        if msg.voltage > (6 * 4.2): # upper limit
+            self.get_logger().error('Battery Overcharged!')
+            msg.power_supply_health = BatteryState.POWER_SUPPLY_HEALTH_OVERVOLTAGE
+        if msg.current > 0: # current flowing into battery
+            self.get_logger().info('Battery Charging.')
+            msg.power_supply_status = BatteryState.POWER_SUPPLY_STATUS_CHARGING
+
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing voltage')
 
 
 def main(args=None):
