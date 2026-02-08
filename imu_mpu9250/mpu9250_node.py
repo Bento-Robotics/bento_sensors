b3a6e0fa1ec15a24a465883fc2e5ab6e9576bb92
bento <tiam.hadjian@gmail.com>
2026-02-08 12:17:56 +0100

    updated sensor -mpu9250 for gyroscop, accolometer, magnetic field and  temperatur

--- imu_mpu9250/mpu9250_node.py
+++ imu_mpu9250/mpu9250_node.py
@@ -35,6 +35,7 @@ class mpu9250_Node(Node):
         # And add a string subscriber
         self.publisher_imu = self.create_publisher(Imu, "imu", 10)
         self.publisher_mag = self.create_publisher(MagneticField, "mag", 10)
+        self.publisher_temp = self.create_publisher(Temperature, "temp", 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
